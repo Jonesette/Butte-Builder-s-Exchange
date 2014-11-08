95d3dcbd280ba58213ab52ae7d0be98971233c5f
Jonesette <sjones@jjaviation.com>
2014-11-08 13:38:20 -0700

    Added Document Links

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+	$("#info").load('info.html');
+	$("#info").load('BBE.html');
+	$("#info").load('MontanaBids.html');
+	$("#info").load('PrivatePR.html');
+	$("#info").load('Newsletter.html');
+});
