25910242a48b50ae84646cddafe5dff4f8d956bc
Jonesette <sjones@jjaviation.com>
2014-12-06 17:40:08 -0700

    Unresponsive Version
    Everything by the Home Page Content

--- app.js
+++ app.js
@@ -1,7 +1,60 @@
 $(document).ready(function () {
+	$("#home").load('home.html');
 	$("#info").load('info.html');
-	$("#info").load('BBE.html');
-	$("#info").load('MontanaBids.html');
-	$("#info").load('PrivatePR.html');
-	$("#info").load('Newsletter.html');
+	$("#Member").load('Member.html');
+	$("#Contact").load('Contact.html');
+
+	$('#Member-button').click(function(){
+
+		$('#home').hide();
+		$('#info').hide();
+		$('#Member').show();
+		$('#Contact').hide();
+	});
+
+	$('#home-button').click(function(){
+
+		$('#home').show();
+		$('#info').hide();
+		$('#Member').hide();
+		$('#Contact').hide();
+	});
+
+	$('#info-button').click(function(){
+		$('#home').hide();
+		$('#info').show();
+		$('#Member').hide();
+		$('#Contact').hide();
+	});
+
+	$('#MontanaBids-button').click(function(){
+		$('#home').hide();
+		$('#info').hide();
+		$('#Member').hide();
+		$('#Contact').hide();
+	});
+
+	$('#PrivatePR-button').click(function(){
+		$('#home').hide();
+		$('#info').hide();
+		$('#Member').hide();
+		$('#Contact').hide();
+	});
+
+	$('#Contact-button').click(function(){
+		$('#home').hide();
+		$('#info').hide();
+		$('#Member').hide();
+		$('#Contact').show();
+	});
+
+	$(".menu").mouseover(function() {
+		$(this).attr('id', 'hover');
+	});
+
+		$(".menu").mouseleave(function() {
+		$(this).removeAttr('id', 'hover');
+	});
+
 });
+
